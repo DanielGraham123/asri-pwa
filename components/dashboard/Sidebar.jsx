87163695259ad7ed33817003551acece1734c391
DanielGraham123 <dg.danielboaz@yahoo.com>
2022-07-30 23:19:33 +0100

    added custom sidebar links

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -71,7 +71,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-900 p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? "translate-x-0" : "-translate-x-64"
         }`}
       >
@@ -126,7 +126,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
               {/* Dashboard */}
               <li
                 className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
-                  pathname === "/dashboard" && "bg-slate-900"
+                  pathname === "/dashboard" && "bg-gray-700 bg-opacity-40"
                 }`}
               >
                 <DashLink href="/dashboard">
@@ -156,7 +156,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z"
                         />
                       </svg>
-                      <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-80 2xl:opacity-100 duration-200">
                         Dashboard
                       </span>
                     </div>
@@ -167,7 +167,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
               {/* Appointments */}
               <li
                 className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
-                  pathname === "/dashboard/appointments" && "bg-slate-900"
+                  pathname === "/dashboard/appointments" &&
+                  "bg-gray-700 bg-opacity-40"
                 }`}
               >
                 <DashLink href="/dashboard/appointments">
@@ -194,7 +195,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           d="M17 23a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 010-2 4 4 0 004-4 1 1 0 012 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1zM7 13a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 110-2 4 4 0 004-4 1 1 0 112 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1z"
                         />
                       </svg>
-                      <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-80 2xl:opacity-100 duration-200">
                         Appointments
                       </span>
                     </div>
@@ -205,7 +206,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
               {/* Patients */}
               <li
                 className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
-                  pathname === "/dashboard/patients" && "bg-slate-900"
+                  pathname === "/dashboard/patients" &&
+                  "bg-gray-700 bg-opacity-40"
                 }`}
               >
                 <DashLink href="/dashboard/patients">
@@ -230,7 +232,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           d="M12 6a3 3 0 110-6 3 3 0 010 6zm2 18h-4a1 1 0 01-1-1v-6H6v-6a3 3 0 013-3h6a3 3 0 013 3v6h-3v6a1 1 0 01-1 1z"
                         />
                       </svg>
-                      <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-80 2xl:opacity-100 duration-200">
                         Patients
                       </span>
                     </div>
@@ -272,7 +274,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                                 <path
                                   className={`fill-current text-slate-400 ${
                                     pathname.includes("settings") &&
-                                    "text-indigo-300"
+                                    "text-indigo-500"
                                   }`}
                                   d="M10.714 18.3c.4-.195.84-.298 1.286-.3a3 3 0 11-3 3c.002-.446.105-.885.3-1.286l-6.007-6.007 1.414-1.414 6.007 6.007z"
                                 />
@@ -291,7 +293,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                                   d="M19.707 9.292a3.012 3.012 0 00-1.415 1.415L13.286 5.7c-.4.195-.84.298-1.286.3a3 3 0 113-3 2.969 2.969 0 01-.3 1.286l5.007 5.006z"
                                 />
                               </svg>
-                              <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                              <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-80 2xl:opacity-100 duration-200">
                                 Settings
                               </span>
                             </div>
