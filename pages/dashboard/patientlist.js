24f1961ed0007d790ff01a38049eb0f9053220e8
DanielGraham123 <dg.danielboaz@yahoo.com>
2022-08-01 18:37:16 +0100

    added patients table with search functionality

---
+++ pages/dashboard/patientlist.js
@@ -0,0 +1,68 @@
+import Image01 from "../../assets/user-36-05.jpg";
+import Image02 from "../../assets/user-36-06.jpg";
+import Image03 from "../../assets/user-36-07.jpg";
+import Image04 from "../../assets/user-36-08.jpg";
+import Image05 from "../../assets/user-36-09.jpg";
+
+export default [
+  {
+    name: "Jane Cooper",
+    phone: "jane.cooper@example.com",
+    title: "Regional Paradigm Technician",
+    treatment: "Optimization",
+    status: "Completed",
+    date: "23/10/2021",
+    phone: "+237 655 293 301",
+    image: Image01,
+  },
+  {
+    name: "Cody Fisher",
+    phone: "cody.fisher@example.com",
+    title: "Product Directives Officer",
+    treatment: "Intranet",
+    status: "Completed",
+    date: "23/10/2021",
+    phone: "+237 655 293 301",
+    image: Image02,
+  },
+  {
+    name: "Esther Howard",
+    phone: "esther.howard@example.com",
+    title: "Forward Response Developer",
+    treatment: "Directives",
+    status: "Completed",
+    date: "23/10/2021",
+    phone: "+237 655 293 301",
+    image: Image03,
+  },
+  {
+    name: "Jenny Wilson",
+    phone: "jenny.wilson@example.com",
+    title: "Central Security Manager",
+    treatment: "Program",
+    status: "Completed",
+    date: "03/10/2017",
+    phone: "+237 655 293 301",
+    image: Image04,
+  },
+  {
+    name: "Kristin Watson",
+    phone: "kristin.watson@example.com",
+    title: "Lean Implementation Liaison",
+    treatment: "Mobility",
+    status: "Completed",
+    date: "23/10/2021",
+    phone: "+237 655 293 301",
+    image: Image05,
+  },
+  // {
+  //   name: "Cameron Williamson",
+  //   phone: "cameron.williamson@example.com",
+  //   title: "Internal Applications Engineer",
+  //   treatment: "Security",
+  //   status: "Completed",
+  // phone: "+237 655 293 301",
+  //   date: "Member",
+  //   image: Image06,
+  // },
+];
