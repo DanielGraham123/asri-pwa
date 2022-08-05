9bbedde7933b6855897a0a7f6f5a2520b97027eb
DanielGraham123 <dg.danielboaz@yahoo.com>
2022-08-05 05:17:32 +0100

    added booklet, account page, and appointments page

--- pages/dashboard/patients/[id]/booklet.js
+++ pages/dashboard/patients/[id]/booklet.js
@@ -194,17 +194,17 @@ export default function Booklet() {
                       <div
                         id="tooltip-dark"
                         role="tooltip"
-                        class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                        className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
                       >
                         Tooltip content
-                        <div class="tooltip-arrow" data-popper-arrow></div>
+                        <div className="tooltip-arrow" data-popper-arrow></div>
                       </div>
                     </p>
                     <div className="flex gap-1 mt-1">
-                      <span class="badge-indigo">
+                      <span className="badge-indigo">
                         H: {illness?.bioData?.height}
                       </span>
-                      <span class="badge-blue">
+                      <span className="badge-blue">
                         W: {illness?.bioData?.weight}
                       </span>
                     </div>
