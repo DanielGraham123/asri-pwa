79f602bfb1d3384c8110e1f7d0081dbd29b0eceb
DanielGraham123 <dg.danielboaz@yahoo.com>
2022-10-06 01:04:58 +0100

    removed outline

--- components/dashboard/boxes/PandemicMap.jsx
+++ components/dashboard/boxes/PandemicMap.jsx
@@ -370,7 +370,7 @@ export default function PatientOverview() {
                         selected
                           ? "border-indigo-500 text-indigo-600"
                           : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                        "whitespace-nowrap py-3 px-2 border-t-4 font-medium text-sm"
+                        "whitespace-nowrap py-3 px-2 border-t-4 font-medium text-sm outline-none"
                       )}
                     >
                       {tab}
